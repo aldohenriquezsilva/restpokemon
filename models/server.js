ebb06888c4d3d8550c2246738bf225093243a2e4
Desarrollo Proycom <desarrolloproycom2@hotmail.com>
2022-03-22 11:23:24 -0300

    [Add] se subre proyecto api rest nodejs

---
+++ models/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+class Server{
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.routes();
+    }
+
+    routes(){
+        this.app.get('/', (req, res) => {
+            res.send('Hola Mundo');
+        })
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port); 
+        });
+    }
+
+}
+
+module.exports = Server;
