2e6ec6941146ba47b06642f27822009c6d2d2df8
Aldo Henriquez <henriquez.aldo@live.cl>
2022-04-04 22:24:53 -0400

    [update] se agrega test hcia pokeApi, pero con parametro vacio

--- test/pokeApi.test.js
+++ test/pokeApi.test.js
@@ -3,8 +3,15 @@ import clients from '../apiServices/clients/pokeApi';
 
 describe('Testing PokeApi.js', () => {
 
-    test('consulting', async ()  => { 
+    test('consulting pika', async ()  => { 
          const result = await clients.performSearch('pika');
          expect(typeof result).toBe('object');
     });
+
+    test('consulting blank', async ()  => { 
+        const result = await clients.performSearch('');
+        expect(typeof result).toBe('object');
+   });
+
 });
+
