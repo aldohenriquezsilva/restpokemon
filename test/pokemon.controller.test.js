2f4b2caf564134adaad6dadfe3cd0790c1453ecc
Aldo Henriquez <henriquez.aldo@live.cl>
2022-04-24 13:34:24 -0400

    [Update] Se agrega test en controller test, para que retornen los code

--- test/pokemon.controller.test.js
+++ test/pokemon.controller.test.js
@@ -1,25 +1,25 @@
  const  controller = require('../apiServices/controllers/pokemon.controller');
 //import pokemonGet from '../apiServices/controllers/pokemon.controller'
+import clients from '../apiServices/clients/pokeApi';
 
-const mockResp200 = {code: 200, data: Array(1)}
-const mockError404 = {code: 404, data: Array(0)}
-const mockError500 = {code: 500, data: Array(0)}
+const mockResp200 = { code: 200, data: Array(1) }
+const mockError404 = { code: 404, data: Array(0) }
+const mockError500 = { code: 500, data: Array(0) }
 
 describe('Testing controller', () => {
   
    test('should return 200', async ()  => {
       const mRes = mockResp200;
-      expect(mRes.code).toEqual(200);        
+      const mResFilt = await clients.performSearch('pika');
+      expect(mRes.code).toEqual(200);      
+      expect(mResFilt.code).toBe(200); 
    },30000);
 
    test('should 404 and return correct value', async () => {   
      const resp = mockError404;
-     expect(resp.code).toEqual(404);     
+     const mResFilt = await clients.performSearch('tester');
+     expect(resp.code).toEqual(404);  
+     expect(mResFilt.code).toBe(404);    
    });
-
-   test('should return 500', async () => {   
-      const resp = mockError500;      
-      expect(resp.code).toEqual(500);      
-    });
           
  });
